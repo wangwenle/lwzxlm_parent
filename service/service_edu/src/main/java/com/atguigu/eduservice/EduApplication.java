715fe72eed9e90143c290c69710c6276ecd9ab05
wangwenle <1334149819@qq.com>
2020-07-23 17:25:32 +0800

    老王装修联盟

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableDiscoveryClient  //nacos注册
+@EnableFeignClients
+@ComponentScan(basePackages = {"com.atguigu"})
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class, args);
+    }
+}
